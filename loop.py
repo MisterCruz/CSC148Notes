3764231c2b07475694fc91e3c2f7c073a02517d2
Rolando Cruz <pspnando@me.com>
2017-02-09 18:49:26 -0500

    :zap:

--- loop.py
+++ loop.py
@@ -14,3 +14,15 @@ def dot_prod(u: [float, ...], v: [float, ...]) -> float:
         total += u[i] * v[i]
 
     return total
+
+#List Comprehension version
+def dot_prodComp(u: [float, ...], v: [float, ...]) -> float:
+    """Return the dot product of u and v
+
+    u, v --- non-empty lists of the same length
+
+    >>> dot_prod([1.0, 2.0], [3.0, 4.0])
+    11.0
+    """
+    # sum of products of pairs of corresponding coordinates of u and v 
+    return sum([u_coord * v_coord for u_coord, v_coord in zip(u, v)])
